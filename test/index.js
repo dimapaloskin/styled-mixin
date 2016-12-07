3110a5437ccc967f41f0e95c9e6573472b99303e
Dmitry <dima@paloskin.me>
2016-12-07 13:45:26 +0300

    [feat] add property test

--- test/index.js
+++ test/index.js
@@ -12,10 +12,25 @@ const tomatoBtnMixin = createMixin`
   color: tomato;
 `;
 
+const testPropertyMixin = createMixin`
+  color: ${props => props.isTomato ? 'tomato' : 'black' };
+`;
+
 test('should add new rule for the styled component', t => {
 
   t.is(Btn.rules.length, 1);
   const TomatoBtn = tomatoBtnMixin(Btn);
   t.is(TomatoBtn.rules.length, 2);
   t.not(TomatoBtn.rules[1].indexOf('color: tomato;'), -1);
+});
+
+test('should add new property rule for the styled component', t => {
+
+  t.is(Btn.rules.length, 1);
+  const TomatoBtn = testPropertyMixin(Btn);
+  t.is(TomatoBtn.rules.length, 4);
+  const isTomatoFn = TomatoBtn.rules[2];
+  t.is(isTomatoFn({ isTomato: true }), 'tomato');
+  t.is(isTomatoFn({ isTomato: false }), 'black');
+  t.is(isTomatoFn({}), 'black');
 });
